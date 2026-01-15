8bbc8993f9e20e274353a2a394d16e28f4c87ad6
jr-k <jessym.reziga@gmail.com>
2026-01-15 14:41:07 +0100

    wip

---
+++ cmd/d4s/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jessym/d4s/internal/ui"
+)
+
+func main() {
+	app := ui.NewApp()
+	if err := app.Run(); err != nil {
+		fmt.Printf("Error running D4s: %v\n", err)
+		os.Exit(1)
+	}
+}
